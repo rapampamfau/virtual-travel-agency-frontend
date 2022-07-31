fea65b00d82af965fe3d20874f41eddb88ff8872
Rafal Szczepanczyk <rafal.szczepanczyk7@gmail.com>
2022-07-31 14:06:17 +0200

    first commit

---
+++ src/main/java/com/frontend/VirtualTravelAgencyFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualTravelAgencyFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirtualTravelAgencyFrontendApplication.class, args);
+    }
+
+}
